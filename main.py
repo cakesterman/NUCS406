337fdc83103dfeba77d2dfd6e3c287d363862315
Tyler Bull <tbtylerbull@gmail.com>
2020-04-28 18:05:55 -0400

    Added a whole bunch of stuff.  Communities and hashtag networks and analyzing them

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import networkx as nx
 from networkx.algorithms.community import k_clique_communities
 from networkx.algorithms.community import greedy_modularity_communities
 import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+import string
+import csv
+import os
 
 
 def read_text_file_and_search_keyword(keyword, file):
@@ -13,11 +18,10 @@ def read_text_file_and_search_keyword(keyword, file):
     user_hashtags = []
     user_sentiments = []
 
-    with open("Data by Day/{}".format(file), encoding='utf8') as datafile:
+    with open(f"Data by Day/{file}", encoding='utf8') as datafile:
 
-        print("Reading in data...")
-
-        # data = csv.reader(csvfile)
+        # print(f"Reading in data from {file[12:len(file)]}...")
+        print(f"Reading in data from {file}...")
 
         line = datafile.readline()
 
@@ -36,14 +40,10 @@ def read_text_file_and_search_keyword(keyword, file):
 
                     if user_tweet is not None:
 
-                        # user_tweet_list.append((user_tweet, counter))
                         user_tweet_list.append(user_tweet)
 
                         mention = add_user_mention(line)
                         hashtags = add_user_hashtags(line)
-
-                        # if any(ele in line for ele in sentiments_list):
-                        #     user_sentiments.append(add_user_sentiment(line))
 
                         # if mention is not None means if the list of mentions is greater than 1
                         if mention is not None:
@@ -63,76 +63,6 @@ def read_text_file_and_search_keyword(keyword, file):
         print("Parsed data in {} seconds".format(end - start))
 
     return user_tweet_list, user_mentions, user_hashtags
-
-
-def read_text_file_search_keyword_by_day(keyword):
-    user_tweet_list = []
-    user_mentions = []
-    user_hashtags = []
-    user_sentiments = []
-
-    # with open("G:/Downloads/twitter-data-timpestamped (1).txt", encoding='utf8') as datafile:
-    # with open("G:/Downloads/twitter-data-timpestamped.txt", encoding='utf8') as datafile:
-    # with open("G:/Downloads/twitter-data-timpestamped (2).txt", encoding='utf8') as datafile:
-    # with open("G:/Downloads/twitter-data-timpestamped-03192020.txt", encoding='utf8') as datafile:
-    with open("C:/Users/Cakesterman/Downloads/day3.txt", encoding='utf8') as datafile:
-
-        print("Reading in data...")
-
-        # data = csv.reader(csvfile)
-
-        line = datafile.readline()
-
-        start = time.perf_counter()
-
-        counter = 1
-        while line:
-
-            # Checks to make sure that the line is a valid line
-            if "Mon " in line or "Tue " in line or "Wed " in line or "Thu " in line or "Fri " in line or "Sat " in line or \
-                    "Sun " in line:
-
-                current_date = line[8:9]
-                current_month = line[4:6]
-
-                # Search day by day
-
-                if keyword in line:
-
-                    user_tweet = add_user(line)
-                    # user_dict = add_user(line)
-
-                    if user_tweet is not None:
-
-                        # user_tweet_list.append((user_tweet, counter))
-                        user_tweet_list.append(user_tweet)
-
-                        mention = add_user_mention(line)
-                        hashtags = add_user_hashtags(line)
-
-                        # if any(ele in line for ele in sentiments_list):
-                        #     user_sentiments.append(add_user_sentiment(line))
-
-                        # if mention is not None means if the list of mentions is greater than 1
-                        if mention is not None:
-                            user_mentions.append(mention)
-
-                        if hashtags is not None:
-                            user_hashtags.append(hashtags)
-
-                        # counter += 1
-
-            # user_tweet_list.append((add_user(line), counter))
-
-            line = datafile.readline()
-
-        end = time.perf_counter()
-
-        print("Parsed data in {} seconds".format(end - start))
-
-    make_network(user_tweet_list, user_mentions, keyword)
-
-    # return user_tweet_list, user_mentions, user_hashtags
 
 
 def add_user(tweet):
@@ -186,17 +116,33 @@ def add_user_hashtags(tweet):
     for x in temp_list[2].split():
 
         if x.startswith('#'):
-            user_hashtags.append(x)
+
+            hashtag = ""
+
+            for letters in x:
+
+                # print(letters)
+
+                if letters != '.' and letters != ',' and letters != ':' and letters != '…' and letters != '?' and letters != '!':
+                    # print(letters)
+                    hashtag += letters
+
+
+            user_hashtags.append(hashtag)
+            # user_hashtags.append(x.translate(str.maketrans('', '', string.punctuation)))
 
     # print(user_hashtags)
 
-    if len(user_hashtags) == 1:
+    #user_hashtags.pop(0)
+
+    if len(user_hashtags) <= 2 or len(user_hashtags) > 4:
 
         return None
 
     else:
 
-        return user_hashtags
+        # print(tuple(user_hashtags))
+        return tuple(user_hashtags)
 
 
 def print_values(passed):
@@ -206,12 +152,25 @@ def print_values(passed):
 
 def main():
 
-    keyword = "coronavirus"
+    keyword = ""
 
-    file_to_read = "Mon_Mar_16.txt"
+    all_data_files = os.listdir("Data by Day/")
 
-    all_user_tweets_corona, all_user_mentions_corona, all_user_hashtags_corona = read_text_file_and_search_keyword(
-        keyword, file_to_read)
+    # file_to_read = "Data by Day/Mon_Apr_06.txt"
+
+    print(all_data_files)
+
+    for file_to_read in all_data_files:
+
+        all_user_tweets_corona, all_user_mentions_corona, all_user_hashtags_corona = read_text_file_and_search_keyword(
+            keyword, file_to_read)
+        make_network_hashtags(all_user_hashtags_corona, file_to_read)
+
+    # file_to_read = "Mon_Mar_16.txt"
+    # all_user_tweets_corona, all_user_mentions_corona, all_user_hashtags_corona = read_text_file_and_search_keyword(
+    #     keyword, file_to_read)
+    #
+    # make_network_hashtags(all_user_hashtags_corona, file_to_read)
 
     # print_values(all_user_tweets_corona)
     # print()
@@ -227,8 +186,11 @@ def main():
     # print()
     # print_values(all_user_hashtags_covid19)
 
-    corona_network = make_network(all_user_tweets_corona, all_user_mentions_corona, keyword)
-    analyze_network(corona_network, all_user_mentions_corona, file_to_read)
+    # corona_network = make_network(all_user_tweets_corona, all_user_mentions_corona, keyword)
+    # # analyze_network(corona_network, all_user_mentions_corona, file_to_read)
+    #
+
+    # analyze_network(corona_hashtag_network, _, all_user_hashtags_corona)
 
 
     # covid_network = make_network(all_user_tweets_covid19, all_user_mentions_covid19, "Covid-19")
@@ -256,9 +218,9 @@ def make_network(all_users, all_user_mentions, keyword):
 
     start = time.perf_counter()
 
-    #nx.draw(corona_network, with_labels=False)
+    # nx.draw(corona_network, with_labels=False)
     #nx.draw_networkx_edges(corona_network, pos=nx.spring_layout(corona_network))
-    #plt.show()
+    # plt.show()
     #plt.savefig("Test")
 
     end = time.perf_counter()
@@ -266,6 +228,99 @@ def make_network(all_users, all_user_mentions, keyword):
     print("Created graph in {} seconds".format(end - start))
 
     return corona_network
+
+def make_network_hashtags(all_user_hashtags, file):
+
+    corona_hashtag_network = nx.Graph()
+
+    # try:
+    #     corona_hashtag_network.add_edges_from(all_user_hashtags)
+    # except:
+    #     pass
+
+    for x in all_user_hashtags:
+
+        try:
+            corona_hashtag_network.add_nodes_from(x[0])
+
+            corona_hashtag_network.add_edge(x[1], x[2])
+
+        except Exception as e:
+            print("FAIL", e)
+
+    print(nx.info(corona_hashtag_network))
+
+    degree_centrality_dict = nx.degree_centrality(corona_hashtag_network)
+    # print(degree_centrality_dict)
+
+    #del degree_centrality_dict["#coronavirus"]
+
+
+    highest_value = 0
+    hashtag = ''
+    for key in degree_centrality_dict:
+
+        if degree_centrality_dict[key] > highest_value:
+            highest_value = degree_centrality_dict[key]
+            hashtag = key
+
+    # Delete everything related to coronavirus
+    # del degree_centrality_dict["#coronavirus"]
+    # del degree_centrality_dict["#COVID19"]
+    # del degree_centrality_dict["#Coronavirus"]
+    # del degree_centrality_dict["#COVIDー19"]
+    # del degree_centrality_dict["#covid19"]
+    # del degree_centrality_dict["#CoronaVirus"]
+    # del degree_centrality_dict["#Covid19"]
+    # del degree_centrality_dict["#COVID2019"]
+    # del degree_centrality_dict["#covidー19uk"]
+
+
+    # print(Counter(degree_centrality_dict).most_common(10))
+    top_10_values = Counter(degree_centrality_dict).most_common(20)
+    print(top_10_values)
+
+    print(f"Highest hashtag {hashtag} with degree centrality of {highest_value}")
+
+    centraility = [x[1] for x in top_10_values]
+    labels = [x[0] for x in top_10_values]
+    y_pos = np.arange(len(labels))
+    # print( [str(x[1]) for x in top_10_values] )
+    print(centraility)
+    print(labels)
+
+    # plt.figure(figsize=(19, 10))
+
+    fig, ax = plt.subplots()
+
+    ax.barh(y_pos, centraility, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(labels)
+    ax.set_xlabel("Degree Centrality")
+    ax.set_title(f"{file[:len(file) - 4]} Top 20 Degree Centrality")
+    plt.savefig(f"Degree Centrality Graphs/Degree Centrality Matplotlib/{file[4:len(file) - 4]} Top 20 Degree Centrality", bbox_inches='tight', dpi=300)
+    plt.show()
+
+
+    # Code for PIE CHART, only shows a wedge tho
+    # fig1, ax1 = plt.subplots()
+    # ax1.pie(sizes, explode=None, labels=labels, autopct='%1.1f%%', startangle=90)
+    # ax1.axis('equal')
+    # plt.axis("equal")
+    # plt.show()
+
+    #with open(f"{file[16:len(file)-3]}_Degree-Centrality_1.csv", 'w', newline='', encoding='utf-8') as csvfile:
+    # with open(f"Degree Centrality Graphs/Degree Centrality CSV/{file[4:len(file) - 3]}_Degree-Centrality_1.csv", 'w', newline='', encoding='utf-8') as csvfile:
+    #
+    #     writer = csv.writer(csvfile)
+    #
+    #     writer.writerow(("Hashtag", "Degree Centrality"))
+    #
+    #     for x in top_10_values:
+    #
+    #         writer.writerow((x[0], x[1]))
+
+    return corona_hashtag_network
 
 
 def analyze_network(network, all_user_mentions, file):
@@ -281,6 +336,8 @@ def analyze_network(network, all_user_mentions, file):
     def analyze_communities():
 
         community = greedy_modularity_communities(network)
+
+        print("Community length:", len(community))
 
         community_list = []
 
@@ -333,11 +390,36 @@ def analyze_network(network, all_user_mentions, file):
 
             plt.show()
 
-
-
         for communities in community_list:
 
-            graph_community(communities)
+            # graph_community(communities)
+            analyze_degree_centrality(communities)
+
+    # Might not use this
+    def analyze_degree_centrality(network):
+
+        community_network = nx.Graph()
+
+        # community_network.add_edges_from(new_mention_list)
+
+        print(nx.info(community_network))
+        # print(nx.info(community_network)[36:38])
+        # print(nx.degree_centrality(community_network))
+
+        degree_centrality_dict = nx.degree_centrality(community_network)
+
+        highest_value = 0
+        hashtag = ''
+        for key in degree_centrality_dict:
+
+            if degree_centrality_dict[key] > highest_value:
+
+                highest_value = degree_centrality_dict[key]
+                hashtag = key
+
+        print(f"Highest hashtag {hashtag} with degree centrality of {highest_value}")
+
+
 
 
         #graph_community()
